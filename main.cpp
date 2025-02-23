39a618235f1ab0195faa0fd424b071856bcefb06
mohammed sabeer <mohammedsabeer079@gmail.com>
2025-02-23 20:34:53 +0530

    modified file

--- main.cpp
+++ main.cpp
@@ -160,9 +160,108 @@ void print14(int n){
     cout<<endl;
   }
 }
+void print15(int n){
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+      cout<<" ";
+    }
+    char ch='A';
+    int breakpoint=(2*i+1)/2;
+    for(int j=0;j<2*i+1;j++){
+      cout<<ch;
+      if(j<breakpoint) ch++;
+      else ch--;
+    }
+    for(int j=0;j<n-i-1;j++){
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+}
+void print16(int n){
+  for(int i=0;i<n;i++){
+    for(char ch='E'-i;ch<='E';ch++){
+      cout<<ch;
+
+    }
+    cout<<endl;
+  }
+}
+void print17(int n){
+  int inis=0;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n-i;j++){
+      cout<<"*";
+    }
+    for(int j=0;j<inis;j++){
+      cout<<" ";
+    }
+    for(int j=0;j<n-i;j++){
+      cout<<"*";
+    }
+    inis+=2;
+    cout<<endl;
+  }
+inis=8;
+for(int i=0;i<n;i++){
+  for(int j=0;j<=i;j++){
+    cout<<"*";
+  }
+  for(int j=0;j<inis;j++){
+    cout<<" ";
+  }
+  for(int j=0;j<=i;j++){
+    cout<<"*";
+  }
+  inis-=2;
+  cout<<endl;
+}
+}
+void print18(int n){
+  int spaces=2*n-2;
+  for(int i=1;i<=2*n-1;i++){
+    int stars=i;
+    if(i>n) stars=2*n-i;
+    for(int j=1;j<=stars;j++){
+      cout<<"*";
+    }
+    for(int j=1;j<=spaces;j++){
+      cout<<" ";
+    }
+    for(int j=1;j<=stars;j++){
+      cout<<"*";
+    }
+    cout<<endl;
+    if(i<n) spaces-=2;
+    else spaces+=2;
+  }
+}
+void print19(int n){
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      if(i==0||j==0||i==n-1||j==n-1)
+      cout<<"*";
+      else
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+}
+void print20(int n){
+  for(int i=0;i<2*n-1;i++){
+    for(int j=0;j<2*n-1;j++){
+      int top=i;
+      int left=j;
+      int right=(2*n-2)-j;
+      int bottom=(2*n-2)-i;
+      cout<<(n-min(min(top,bottom),min(left,right)));
+    }
+    cout<<endl;
+  }
+}
 int main(){
  int n;
  cin>>n;
- print14(n);  
+ print20(n);
 }
    
